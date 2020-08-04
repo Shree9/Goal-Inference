0f6e0e3d597437cb19ab8a64eb0321ed5f655e04
Shree9 <gotteti.2@wright.edu>
2020-08-04 16:25:26 -0400

    Update SentenceSimilarity.py

--- MIDCA/midca/SentenceSimilarity.py
+++ MIDCA/midca/SentenceSimilarity.py
@@ -2,60 +2,72 @@
 # two sentences using cosine similarity.
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import string
 
-# X = input("Enter first string: ").lower()
-# Y = input("Enter second string: ").lower()
-X ="Stack the red block on the blue block."
-# X ="Pick the red block."
-# X ="Unstack the red block on the blue block."
-# Y ="Let the red block be placed on the blue block."
-Y = "Place the red block on top of the blue block."
-# Y = "Deposit red block on the blue block."
-# Y = "Connect the bottom of the red block to the top of the blue block."
-# Y = "Connect red block to the top of the blue block."
-# Y = "Connect red block to the blue block."
-# Y = "Collapse the red block from the blue block"
-# Y = "Let red and blue blocks be stacked."
-# Y = "Put the red and blue blocks together."
-# Y = "Bring the red block to the blue block."
-# Y = "Can you stack the red block on the blue block?"
-# Y = "Can you find the red block and blue block and then put them together?"
+
+database = {
+              "stack": "stack the blue block on the green block",
+              "unstack": "let the green block be separated from the blue block",
+              # "unclutter the green block and blue block",
+               #"detach the blue block from the green block",
+              "pickup": "pick the green block",
+              "putdown": "please put down the green block"
+            }
+# Y1
+Y = "Can you find the green block, and blue block and then put them on each other?"
+# Y2
 # Y = "Now, the red block is on the blue block."
-# Y ="Build a tower."
-# Y = "Build a tower using red and blue blocks."
-# Y = "arrange the red block on the blue block."
-# Y= "Unstack the red block on the blue block."
-# Y = "Build a tower with red and blue blocks."
-# Y = "Elevate the red block and place it on the blue block."
+# Y3
+# Y = "Connect the blue block to the green block."
+# Y4
+# Y = "Arrange the blue block on the green block."
+# Y5
+# Y = "Elevate the blue block and place it on the green block."
+# Y6
+# Y = "Build a tower with green and blue blocks."
+# Y7
+# Y = "Detach the bottom of the blue block from the top of the green block."
+# Y8
+# Y = "Put the green block on your hand"
+# Y9
+# Y = "Can the green block be lifted?"
 
-# X = "I am thirsty."
-# Y = "I need some water."
-# Y = "I need coffee."
-# Y = "My mouth is dry."
+# Y = "stack the green block on the blue block"
+# Y = "let the green block be separated from the blue block"
+# Y = "remove the green block from the blue block"
 
-# tokenization
-X_list = word_tokenize(X)
-Y_list = word_tokenize(Y)
+Y = Y.lower()
 
-# sw contains the list of stopwords
-sw = stopwords.words('english')
-l1 =[];l2 =[]
+for X in database.values():
+    print("-------------------------------")
+    print(X)
+    print(Y)
 
-# remove stop words from the string
-X_set = {w for w in X_list if not w in sw}
-Y_set = {w for w in Y_list if not w in sw}
+    # tokenization
+    X_list = word_tokenize(X)
+    Y_list = word_tokenize(Y)
 
-# form a set containing keywords of both strings
-rvector = X_set.union(Y_set)
-for w in rvector:
-    if w in X_set: l1.append(1) # create a vector
-    else: l1.append(0)
-    if w in Y_set: l2.append(1)
-    else: l2.append(0)
-c = 0
 
-# cosine formula
-for i in range(len(rvector)):
+    # sw contains the list of stopwords
+    sw = stopwords.words('english')
+    l1 =[];l2 =[]
+
+    # remove stop words from the string
+    X_set = {w for w in X_list if not w in sw}
+    Y_set =  {w for w in Y_list if not w in sw}
+
+    # form a set containing keywords of both strings
+    rvector = X_set.union(Y_set)
+    for w in rvector:
+        if w in X_set: l1.append(1) # create a vector
+        else: l1.append(0)
+        if w in Y_set: l2.append(1)
+        else: l2.append(0)
+
+    c = 0
+
+    # cosine formula
+    for i in range(len(rvector)):
         c+= l1[i]*l2[i]
-cosine = c / float((sum(l1)*sum(l2))**0.5)
-print("similarity: ", cosine)
+    cosine = c / float((sum(l1)*sum(l2))**0.5)
+    print("similarity: ", cosine)
